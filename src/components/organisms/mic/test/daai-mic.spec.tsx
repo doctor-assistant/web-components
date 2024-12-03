aa1d63225ad9f14b0a624dfaadcd10638c1728b9
vitoria.cst <vitoria.cst.holanda@gmail.com>
2024-12-03 20:07:46 -0300

    test: :test_tube: adjusting the tests

--- src/components/organisms/mic/test/daai-mic.spec.tsx
+++ src/components/organisms/mic/test/daai-mic.spec.tsx
@@ -15,9 +15,6 @@ describe('daai-mic', () => {
           <div class="flex items-center justify-center">
             <daai-text text="Aguardando autorização do microfone"></daai-text>
           </div>
-          <daai-button-with-icon id="config-mic">
-            <daai-config-mic-icon></daai-config-mic-icon>
-          </daai-button-with-icon>
         </div>
       </daai-mic>
     `);
