69230437207096c56e8beb8169fcdfc38c6686aa
vitoria.cst <vitoria.cst.holanda@gmail.com>
2025-03-24 18:23:48 -0300

    fix: :zap: add try/ catch

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,43 +1,54 @@
 export const isMobile = () => {
-  const userAgent = navigator.userAgent;
-  return /Mobi|Android|iPhone/i.test(userAgent);
+  try{
+    const userAgent = navigator.userAgent;
+    return /Mobi|Android|iPhone/i.test(userAgent);
+  } catch(error) {
+    console.error(error)
+  }
 }
 
 export const getOSInfo = () => {
-  const userAgent = navigator.userAgent;
+  try {
+    const userAgent = navigator.userAgent;
 
-  if (userAgent.includes("Windows")) {
-    return "Windows";
-  } else if (userAgent.includes("Mac")) {
-    return "MacOS";
-  } else if (userAgent.includes("Linux")) {
-    return "Linux";
-  } else if (userAgent.includes("Android")) {
-    return "Android";
-  } else if (userAgent.includes("iOS")) {
-    return "iOS";
+    if (userAgent.includes("Windows")) {
+      return "Windows";
+    } else if (userAgent.includes("Mac")) {
+      return "MacOS";
+    } else if (userAgent.includes("Linux")) {
+      return "Linux";
+    } else if (userAgent.includes("Android")) {
+      return "Android";
+    } else if (userAgent.includes("iOS")) {
+      return "iOS";
+    }
+
+    return "Unknown";
+  }  catch(error) {
+    console.error(error)
   }
-
-  return "Unknown";
 }
 
 export const getBrowserInfo = () => {
-  const userAgent = navigator.userAgent;
+  try{
+    const userAgent = navigator.userAgent;
+    if (userAgent.includes("Firefox")) {
+      return "Firefox";
+    } else if (userAgent.includes("Chrome") && !userAgent.includes("Edg")) {
+      return "Chrome";
+    } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+      return "Safari";
+    } else if (userAgent.includes("Edg")) {
+      return "Edge";
+    } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+      return "Opera";
+    } else if (userAgent.includes("MSIE") || userAgent.includes("Trident")) {
+      return "Internet Explorer";
+    }
 
-  if (userAgent.includes("Firefox")) {
-    return "Firefox";
-  } else if (userAgent.includes("Chrome") && !userAgent.includes("Edg")) {
-    return "Chrome";
-  } else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
-    return "Safari";
-  } else if (userAgent.includes("Edg")) {
-    return "Edge";
-  } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
-    return "Opera";
-  } else if (userAgent.includes("MSIE") || userAgent.includes("Trident")) {
-    return "Internet Explorer";
+    return "Unknown";
+  } catch(error) {
+    console.error(error)
   }
-
-  return "Unknown";
 }
 
